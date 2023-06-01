720f38d71ca6c8cbbee97df93e750d7a057c0097
saurabhjha137 <saurabhjha137@gmail.com>
2023-06-01 16:55:38 +0530

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Card {
+
+    private final String rank;
+    private final String suit;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+}
